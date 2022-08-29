333914bb27ae183af6ca00e22e5526433e1a2e0d
ricardo <ricardohitalo1@gmail.com>
2022-08-29 17:28:46 -0300

    connection API DataBase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
+import { typeOrmConfig } from './configs/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [],
   providers: [],
 })
